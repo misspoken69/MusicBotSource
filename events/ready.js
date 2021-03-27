224cab2dd873a0277aaff14bab6471a921f1386c
Alex <74885892+misspoken69@users.noreply.github.com>
2021-03-26 21:12:30 -0700

    Create ready.js

---
+++ events/ready.js
@@ -0,0 +1,7 @@
+module.exports = (client) => {
+    console.log('Login as' + client.user.tag)
+    console.log(`MisspokenMusic`);
+    console.log(`MisspokenMusic`);
+    console.log(`MisspokenMusic`);
+    console.log(`MisspokenMusic`);
+}
