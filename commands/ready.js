310ad85f1359e4885b7a1f6fc765011ed02b49ae
Alex <74885892+misspoken69@users.noreply.github.com>
2021-03-26 21:14:06 -0700

    Added all command files

---
+++ commands/ready.js
@@ -0,0 +1,17 @@
+module.exports = (client) => {
+    console.log('Login as' + client.user.tag)
+    console.log(`YassineHz ©`);
+    console.log(`YassineHz ©`);
+    console.log(`YassineHz ©`);
+    console.log(`YassineHz ©`);
+    console.log(`YassineHz ©`);
+    console.log(`YassineHz ©`);
+    console.log(`YassineHz ©`);
+    console.log(`YassineHz ©`);
+    console.log(`YassineHz ©`);
+    console.log(`YassineHz ©`);
+    console.log(`YassineHz ©`);
+    console.log(`YassineHz ©`);
+    console.log(`YassineHz ©`);
+    console.log(`YassineHz ©`);
+}
